5c5ba8b1a289c941b5dd1374d85b8d9f9163368e
phemanuel <emmakinyooye@gmail.com>
2025-03-02 13:10:36 -0800

    Completed the landing page.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -150,7 +150,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/admin');
 
 
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,19 +40,19 @@
 
   <header id="header" class="header d-flex align-items-center sticky-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center">
-
+    <img src="{{asset('college/avatar.png')}}" alt=""> <h1 class="sitename"></h1>
       <a href="#" class="logo d-flex align-items-center me-auto">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <img src="{{asset('college/avatar.png')}}" alt=""> <h1 class="sitename">EGI</h1>
+        
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
           <li><a href="#" class="active">Home<br></a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Courses</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
@@ -123,28 +123,28 @@ We empower individuals, organizations, and communities to achieve excellence and
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="500" data-purecounter-duration="1" class="purecounter"></span>
               <p>Students</p>
             </div>
           </div><!-- End Stats Item -->
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="10" data-purecounter-duration="1" class="purecounter"></span>
               <p>Courses</p>
             </div>
           </div><!-- End Stats Item -->
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
               <p>Events</p>
             </div>
           </div><!-- End Stats Item -->
 
           <div class="col-lg-3 col-md-6">
             <div class="stats-item text-center w-100 h-100">
-              <span data-purecounter-start="0" data-purecounter-end="24" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="5" data-purecounter-duration="1" class="purecounter"></span>
               <p>Trainers</p>
             </div>
           </div><!-- End Stats Item -->
@@ -222,13 +222,14 @@ We empower individuals, organizations, and communities to achieve excellence and
     </section><!-- /Features Section -->
 
     <!-- Courses Section -->
-    <section id="courses" class="courses section">
+    <section id="services" class="courses section">
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Students</h2>
-        <p>Testimonies</p>
-      </div><!-- End Section Title -->
+        <h2>Our</h2>
+        <p>Services</p>
+      </div>
+      <!-- End Section Title -->
 
       <div class="container">
 
@@ -238,14 +239,21 @@ We empower individuals, organizations, and communities to achieve excellence and
             <div class="course-item">
               <img src="{{asset('web/assets/img/course-1.jpg')}}" class="img-fluid" alt="...">
               <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
+                <!-- <div class="d-flex justify-content-between align-items-center mb-3">
                   <p class="category">Web Development</p>
                   <p class="price">$169</p>
-                </div>
+                </div> -->
 
-                <h3><a href="course-details.html">Website Design</a></h3>
-                <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
+                <h3><a href="#">Short Term Courses</a></h3>
+                <p>We offer short-term courses designed to equip you with practical, industry-relevant skills in a short period. Whether you're looking to boost your career, switch industries, or gain specialized knowledge, 
+                  our flexible programs provide hands-on training that aligns with global standards.</p>
+                  <p>
+                  ✔ Fast-Track Learning – Gain valuable skills in just weeks or months.<br>
+✔ Affordable & Accessible – Quality education at a cost-effective price.<br>
+✔ Flexible Schedules – Learn at your pace, online or in-person.<br>
+✔ Career-Focused – Industry-aligned curriculum for job readiness.
+                  </p>
+                <!-- <div class="trainer d-flex justify-content-between align-items-center">
                   <div class="trainer-profile d-flex align-items-center">
                     <img src="{{asset('web/assets/img/trainers/trainer-1-2.jpg')}}" class="img-fluid" alt="">
                     <a href="" class="trainer-link">Antonio</a>
@@ -255,7 +263,7 @@ We empower individuals, organizations, and communities to achieve excellence and
                     &nbsp;&nbsp;
                     <i class="bi bi-heart heart-icon"></i>&nbsp;65
                   </div>
-                </div>
+                </div> -->
               </div>
             </div>
           </div> <!-- End Course Item-->
@@ -264,14 +272,22 @@ We empower individuals, organizations, and communities to achieve excellence and
             <div class="course-item">
               <img src="{{asset('web/assets/img/course-2.jpg')}}" class="img-fluid" alt="...">
               <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
+                <!-- <div class="d-flex justify-content-between align-items-center mb-3">
                   <p class="category">Marketing</p>
                   <p class="price">$250</p>
-                </div>
+                </div> -->
 
-                <h3><a href="course-details.html">Search Engine Optimization</a></h3>
-                <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
+                <h3><a href="#">Community Health Services</a></h3>
+                <p>We are committed to improving public health through our Community Health Services. Our expert team provides health education, 
+                  wellness programs, and preventive care to ensure healthier communities.</p>
+                  <p>
+                  ✔ Health Awareness Campaigns – Educating communities on disease prevention and healthy living.<br>
+✔ Maternal & Child Health Support – Providing essential care and guidance for mothers and children.<br>
+✔ Emergency First Aid & Basic Healthcare – Offering first aid training and health screenings.<br>
+✔ Nutrition & Wellness Programs – Promoting balanced diets and healthy lifestyles.<br>
+✔ Community Outreach & Health Advocacy – Partnering with organizations to improve public health.
+                  </p>
+                <!-- <div class="trainer d-flex justify-content-between align-items-center">
                   <div class="trainer-profile d-flex align-items-center">
                     <img src="{{asset('web/assets/img/trainers/trainer-2-2.jpg')}}" class="img-fluid" alt="">
                     <a href="" class="trainer-link">Lana</a>
@@ -281,7 +297,7 @@ We empower individuals, organizations, and communities to achieve excellence and
                     &nbsp;&nbsp;
                     <i class="bi bi-heart heart-icon"></i>&nbsp;42
                   </div>
-                </div>
+                </div> -->
               </div>
             </div>
           </div> <!-- End Course Item-->
@@ -290,14 +306,22 @@ We empower individuals, organizations, and communities to achieve excellence and
             <div class="course-item">
               <img src="{{asset('web/assets/img/course-3.jpg')}}" class="img-fluid" alt="...">
               <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
+                <!-- <div class="d-flex justify-content-between align-items-center mb-3">
                   <p class="category">Content</p>
                   <p class="price">$180</p>
-                </div>
+                </div> -->
 
-                <h3><a href="course-details.html">Copywriting</a></h3>
-                <p class="description">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
+                <h3><a href="course-details.html">Skill Acquisition Program</a></h3>
+                <p>We empower individuals with practical skills to enhance their career opportunities and entrepreneurial success. Our Skill Acquisition Program is designed to equip participants with 
+                  hands-on expertise in various fields, fostering self-reliance and financial independence.</p>
+                  <p>
+                  ✔ Entrepreneurial & Vocational Training – Learn profitable skills for personal and professional growth.<br>
+✔ Digital & Tech Skills – Gain expertise in areas like web design, digital marketing, and IT essentials.<br>
+✔ Handcraft & Creative Arts – Develop skills in fashion design, bead making, and craftwork.<br>
+✔ Business & Financial Literacy – Understand financial management, business startup strategies, and more.<br>
+✔ Soft Skills Development – Improve communication, leadership, and problem-solving abilities.
+                  </p>
+                <!-- <div class="trainer d-flex justify-content-between align-items-center">
                   <div class="trainer-profile d-flex align-items-center">
                     <img src="{{asset('web/assets/img/trainers/trainer-3-2.jpg')}}" class="img-fluid" alt="">
                     <a href="" class="trainer-link">Brandon</a>
@@ -307,7 +331,7 @@ We empower individuals, organizations, and communities to achieve excellence and
                     &nbsp;&nbsp;
                     <i class="bi bi-heart heart-icon"></i>&nbsp;85
                   </div>
-                </div>
+                </div> -->
               </div>
             </div>
           </div> <!-- End Course Item-->
@@ -316,10 +340,94 @@ We empower individuals, organizations, and communities to achieve excellence and
 
       </div>
 
-    </section><!-- /Courses Section -->
+    </section>
+    <!-- /Courses Section -->
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+        <h2>Our</h2>
+        <p>Contact</p>
+      </div>
+      <!-- End Section Title -->
+      <!-- <div class="mb-5" data-aos="fade-up" data-aos-delay="200">
+        <iframe style="border:0; width: 100%; height: 300px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div> -->
+      <!-- End Google Maps -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4">
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+              <i class="bi bi-geo-alt flex-shrink-0"></i>
+              <div>
+                <h3>Address</h3>
+                <p>PLOT 7B OREMEJI STREET, PAKOTO, OGUN STATE.</p>
+              </div>
+            </div><!-- End Info Item -->
+
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+              <i class="bi bi-telephone flex-shrink-0"></i>
+              <div>
+                <h3>Call Us</h3>
+                <p>+2348167962854; +2349159488707</p>
+              </div>
+            </div><!-- End Info Item -->
+
+            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="500">
+              <i class="bi bi-envelope flex-shrink-0"></i>
+              <div>
+                <h3>Email Us</h3>
+                <p>info@excellerglobal.com</p>
+              </div>
+            </div><!-- End Info Item -->
+
+          </div>
+
+          <div class="col-lg-8">
+            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+              <div class="row gy-4">
+
+                <div class="col-md-6">
+                  <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
+                </div>
+
+                <div class="col-md-6 ">
+                  <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+
+                  <button type="submit">Send Message</button>
+                </div>
+
+              </div>
+            </form>
+          </div><!-- End Contact Form -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Contact Section -->
+
 
     <!-- Trainers Index Section -->
-    <section id="trainers-index" class="section trainers-index">
+    <!-- <section id="trainers-index" class="section trainers-index">
 
       <div class="container">
 
@@ -342,7 +450,7 @@ We empower individuals, organizations, and communities to achieve excellence and
                 </div>
               </div>
             </div>
-          </div><!-- End Team Member -->
+          </div>
 
           <div class="col-lg-4 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="200">
             <div class="member">
@@ -361,7 +469,7 @@ We empower individuals, organizations, and communities to achieve excellence and
                 </div>
               </div>
             </div>
-          </div><!-- End Team Member -->
+          </div>
 
           <div class="col-lg-4 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="300">
             <div class="member">
@@ -380,13 +488,14 @@ We empower individuals, organizations, and communities to achieve excellence and
                 </div>
               </div>
             </div>
-          </div><!-- End Team Member -->
+          </div>
 
         </div>
 
       </div>
 
-    </section><!-- /Trainers Index Section -->
+    </section> -->
+    <!-- /Trainers Index Section -->
 
   </main>
 
@@ -396,13 +505,13 @@ We empower individuals, organizations, and communities to achieve excellence and
       <div class="row gy-4">
         <div class="col-lg-4 col-md-6 footer-about">
           <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">EGI</span>
+            <span class="sitename">Exceller Global Institute</span>
           </a>
           <div class="footer-contact pt-3">
             <p>PLOT 7B OREMEJI STREET, PAKOTO,</p>
             <p>OGUN STATE</p>
             <p class="mt-3"><strong>Phone:</strong> <span>+2348167962854; +2349159488707</span></p>
-            <p><strong>Email:</strong> <span>info@excellerinstitute.org</span></p>
+            <p><strong>Email:</strong> <span>info@excellerglobal.com</span></p>
           </div>
           <div class="social-links d-flex mt-4">
             <a href=""><i class="bi bi-twitter-x"></i></a>
